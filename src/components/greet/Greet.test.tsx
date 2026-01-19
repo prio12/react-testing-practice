9573f1d57344f757c7241d16e4d73c03fa7123b4
prio12 <maksudurprio.mp@gmail.com>
2026-01-19 10:47:55 +0600

    learned about grouping tests with describe keyword

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -2,14 +2,16 @@
 // !Test 1: renders "Hello"
 // ! Test 2: renders "Hello {name}" if a name prop is passed
 
-import { expect, test } from 'vitest';
+import { describe, expect, test } from 'vitest';
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-test('renders Hello', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
+describe('Greet Component', () => {
+  test('renders Hello', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 });
 
 test('renders Hello with a Name props', () => {
